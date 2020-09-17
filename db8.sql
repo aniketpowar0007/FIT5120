d4774131f3d955e2cb319be68c12e543e5ea3cab
njer0001 <63134325+njer0001@users.noreply.github.com>
2020-09-17 19:16:11 +1000

    Add files via upload
    iteration2

---
+++ db8.sql
@@ -0,0 +1,9 @@
+CREATE TABLE db_Service (
+  db4_id BIGINT,
+  State varchar(20),
+  Service_Providers varchar(500),
+  Suburb varchar(10000),
+  Star_Rating int(2),
+  Website varchar(10000),
+  PRIMARY KEY (db4_id)
+);
